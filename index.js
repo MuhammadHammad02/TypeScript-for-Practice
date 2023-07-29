b0eb64b7f86fbce6e9d51e9dae87fcc99f0a0d5d
Muhammad Hammad <108385539+MuhammadHammad02@users.noreply.github.com>
2023-07-30 00:24:24 +0500

    seventh commit
    Explicit Casting in Typescript

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-let obj1 = {
-    student_id: 3232,
-    name: "rita",
-    teacher_Id: 7873,
-    teacher_name: "seema",
-};
-console.log(obj1.teacher_Id);
-console.log(obj1.name);
+let myname = "Hammad";
+console.log(myname.length);
+console.log(myname.length);
 export {};
