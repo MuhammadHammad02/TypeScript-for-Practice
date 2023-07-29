191a4a98a1ce040cd05ce27223afe998cca82d3d
Muhammad Hammad <108385539+MuhammadHammad02@users.noreply.github.com>
2023-07-29 23:23:21 +0500

    sixth commit
    Intersection type in typescript

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// nested object 
-const myBook = {
-    author: {
-        firstName: "Hammad",
-        lastName: "Khan"
-    },
-    name: "My Best Book"
+let obj1 = {
+    student_id: 3232,
+    name: "rita",
+    teacher_Id: 7873,
+    teacher_name: "seema",
 };
+console.log(obj1.teacher_Id);
+console.log(obj1.name);
 export {};
--- index.ts
+++ index.ts
@@ -1,22 +1,21 @@
-// nested object 
-
-type Author = {
-    firstName: string;
-    lastName: string;
-};
-
-type Book = {
-    author: Author;
+interface Student {
+    student_id: number;
     name: string;
-};
-
-const myBook : Book = {
-    author: {
-        firstName: "Hammad",
-        lastName: "Khan"
-    },
+  }
     
-        name: "My Best Book"
-    
-
+interface Teacher {
+    teacher_Id: number;
+    teacher_name: string;
 }
+    
+type intersected_type = Student & Teacher;
+    
+let obj1: intersected_type = {
+    student_id: 3232,
+    name: "rita",
+    teacher_Id: 7873,
+    teacher_name: "seema",
+};
+    
+console.log(obj1.teacher_Id);
+console.log(obj1.name);
