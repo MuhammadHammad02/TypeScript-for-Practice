3c06f4ba2506dc3c1a92caad36e98b2d96afd8a2
Muhammad Hammad <108385539+MuhammadHammad02@users.noreply.github.com>
2023-07-30 00:39:54 +0500

    eighth commit
    Enum in Typescript

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
-let myname: unknown = "Hammad";
-console.log((myname as string).length);
-console.log((<string> myname).length)
+enum Color  {Red, Green, Blue};
+var c: Color = Color.Green
+
+console.log(c)
+
+enum Color1 {Red = 1 , Green, Blue};
+
+var colorName: string = Color1[2]
+
+console.log(colorName)
+
+enum Color2 {Red = 1 , Green = 2, Blue = 4}
+
+var colorIndex = Color2["Blue"]
+
+console.log(colorIndex)
