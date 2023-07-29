834dabc02519142b620ab825aec6bc31ee15b69e
Muhammad Hammad <108385539+MuhammadHammad02@users.noreply.github.com>
2023-07-29 14:54:01 +0500

    initial commit
    typescript basics

---
+++ index.js
@@ -0,0 +1,53 @@
+/*console.log("Hello World!")
+
+let message = "My name is Hammad"
+
+console.log(message)
+
+// strongly types syntax
+
+let a: string = "Pakistan";
+
+let b: number = 9;
+
+let c: boolean = true;
+
+// type inference
+
+let d = "USA";
+
+let e = 10.9;
+
+let f = false;
+
+a = "Canada";
+
+e = 222;
+
+f= true
+
+// use const where value will not be changed
+
+const h = 5;
+const g: number = 33;
+const i = 'best';
+
+// use let instead of var
+// because letv has blocked scope
+
+if(true){
+    let z = 4;
+}
+else{
+    let z = "string";
+}
+console.log("let: " + z )
+
+*/
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([{
+        name: 'age',
+        tyep: 'number',
+        message: "Kindly enter age"
+    }]);
+console.log("Insha Allah, in " + (60 - answers.age) + "years  you will be 60 years old");
